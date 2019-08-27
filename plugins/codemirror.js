06912175f27a0fd082095d0c7995e736cc462dfc
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2019-08-27 22:42:20 +0100

    :heavy_plus_sign: Add codemirror, vue-codemirror, and @flow-lang/framework dependencies.

---
+++ plugins/codemirror.js
@@ -0,0 +1,16 @@
+// require lib
+import Vue from 'vue'
+import VueCodemirror from 'vue-codemirror'
+
+// require styles
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/javascript/javascript.js'
+import 'codemirror/theme/base16-dark.css'
+
+// require more codemirror resource...
+
+// you can set default global options and events when use
+Vue.use(VueCodemirror, /* { 
+  options: { theme: 'base16-dark', ... },
+  events: ['scroll', ...]
+} */)
