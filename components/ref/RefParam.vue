a845d520629f45dd9571c0a3d07d4a0802bbee23
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2019-09-17 11:56:15 +0100

    :construction: Writing API reference.

---
+++ components/ref/RefParam.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="{-spacing ml-4 pb-1 mb-2 border-b -}">
+    <span class="{-typography text-black font-bold text-md font-mono -} {-background bg-gray-200 -} {-border border-l-4 border-gray-900 rounded-r -} {-spacing pl-6 pr-2 mr-2 -}">
+      {{ param }}
+    </span>
+
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [ 'param' ]
+}
+</script>
--- pages/api/program.vue
+++ pages/api/program.vue
@@ -3,46 +3,130 @@
     <content-heading>Program</content-heading>
 
     <!-- Program.instrument --------------------------------------------------->
-    <ref-module anchor="Program.instrument">
-      Program.instrument
-    </ref-module>
+    <content-section>
+      <ref-module anchor="Program.instrument">
+        Program.instrument
+      </ref-module>
 
-    <ref-constructor anchor="Program.instrument_Constructor">
-      Constructor
-    </ref-constructor>
+      An instrument program is a full Flow application with both and audio and
+      visual element.
 
-    <ref-heading anchor="Program.instrument_Parameters">
-      Parameters
-    </ref-heading>
+      <ref-constructor anchor="Program.instrument_Constructor">
+        Constructor
+      </ref-constructor>
 
-    <ref-heading anchor="Program.instrument_Methods">
-      Methods
-    </ref-heading>
+      Constructs a new Flow instrument application.
 
-    <ref-heading anchor="Program.instrument_Example">
-      Example
-    </ref-heading>
+      <content-code>
+        const App = Program.instrument(init, update, audio, view, listen)
+      </content-code>
+
+      <ref-heading anchor="Program.instrument_Parameters">
+        Parameters
+      </ref-heading>
+
+      <ref-param :param="'init'">
+        
+      </ref-param>
+
+      <ref-param :param="'update'">
+        hello
+      </ref-param>
+
+      <ref-param :param="'audio'">
+        hello
+      </ref-param>
+
+      <ref-param :param="'view'">
+        hello
+      </ref-param>
+
+      <ref-param :param="'listen'">
+        hello
+      </ref-param>
+
+      <ref-heading anchor="Program.instrument_Methods">
+        Methods
+      </ref-heading>
+
+      <ref-param :param="'start'">
+        Starts the application. Nothing will render before calling this method.
+        An object with three properties must be passed in; (1) a node property
+        with a reference to the DOM node to mount to, (2) a context property
+        with a reference to an AudioContext, and (3) a flags object which gets
+        passed to the init method.
+
+        <content-code>
+          App.start({
+            node: document.querySelector('#app'),
+            context: new AudioContext(),
+            flags: {
+              foo: 'bar'
+            }
+          })
+        </content-code>
+      </ref-param>
+
+      <ref-param :param="'use'">
+        Register a plugin for the application to use. Any calls to the use method
+        must be done before calling start.
+
+        <content-code>
+          App.use(DOM.Event)
+        </content-code>
+      </ref-param>
+
+      <ref-param :param="'send'">
+        Send an action to the runtime. Useful if you need to integrate some
+        external javascript without making use of Flow's plugin system.
+
+        <content-code>
+          App.send({
+            action: 'do-something ',
+            payload: {
+              hello: 'world'
+            }
+          })
+        </content-code>
+      </ref-param>
+
+      <ref-heading anchor="Program.instrument_Example">
+        Example
+      </ref-heading>
+
+      <content-code>
+        const App = Program.instrument(init, update, audio, view, listen)
+
+        App.use(DOM.Event)
+        App.use(Audio.Event)
+
+        App.start({
+          node: document.querySelector('#app'),
+          context: new AudioContext(),
+          flags: {}
+        })
+      </content-code>
+    </content-section>
 
     <!-- Program.worker ------------------------------------------------------->
-    <ref-module anchor="Program.worker">
-      Program.worker
-    </ref-module>
+    <content-section>
+      <ref-module anchor="Program.worker">
+        Program.worker
+      </ref-module>
 
-    <ref-constructor anchor="Program.worker_Constructor">
-      Constructor
-    </ref-constructor>
 
-    <ref-heading anchor="Program.worker_Parameters">
-      Parameters
-    </ref-heading>
+      <ref-constructor anchor="Program.worker_Constructor">
+        Constructor
+      </ref-constructor>
 
-    <ref-heading anchor="Program.worker_Methods">
-      Methods
-    </ref-heading>
+      <ref-heading anchor="Program.worker_Parameters">
+        Parameters
+      </ref-heading>
 
-    <ref-heading anchor="Program.worker_Example">
-      Example
-    </ref-heading>
+      <ref-heading anchor="Program.worker_Example">
+        Example
+      </ref-heading>
+    </content-section>
 
     <div class="{-typography text-right text-indigo-500 hover:text-indigo-800 -} {-spacing my-4 -}">
       <nuxt-link to="/api/audio">API: Audio</nuxt-link> »
@@ -60,6 +144,7 @@
   import RefModule from '~/components/ref/RefModule'
   import RefConstructor from '~/components/ref/RefConstructor'
   import RefHeading from '~/components/ref/RefHeading'
+  import RefParam from '~/components/ref/RefParam'
 
   export default {
     components: {
@@ -71,7 +156,8 @@
       //
       RefModule,
       RefConstructor,
-      RefHeading
+      RefHeading,
+      RefParam
     }
   }
 </script>
